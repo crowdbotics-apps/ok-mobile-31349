9e044114da4efe45b31bc250456c4b56b2f05fa3
Crowdbotics <team@crowdbotics.com>
2021-10-12 23:13:58 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import School
+from .serializers import SchoolSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,12 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    serializer_class = SchoolSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = School.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+
+class School(models.Model):
+    "Generated Model"
+    levels = models.CharField(
+        max_length=256,
+    )
+    teachers = models.CharField(
+        max_length=256,
+    )
